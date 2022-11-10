936bd308f539e1abdabb541de9eb183ef674f3d7
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2022-11-10 17:40:37 +0100

    Removed tests

--- tests/run_tests_full.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-rm "../*.report*" "../*.test*" "../*.error"
-
-python3 Test.py -f -i test.txt -c ../SD_Group11.circ --logisimpath ../logisim.jar
-
-rm "../*.report*" "../*.test*" "../*.error*"
